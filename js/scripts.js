6178268c5905914a5f228caebf63e67807d05eb1
Jessica Xia <jessicax@uchicago.edu>
2020-08-28 00:24:36 -0700

    projects progress and loading icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
- $(document).ready(function () {
-    $('#past').hide();
-    $('#current').hide();
-    $(document).on('click', '#complete', function () {
-        this.style.color = '#cdc6a5';
-        $('#progress').css('color', '#424b54');
-        $('#past').show();
-        $('#current').hide();
-    });
-    $(document).on('click', '#progress', function () {
-        this.style.color = '#cdc6a5';
-        $('#complete').css('color', '#424b54');
-        $('#current').show();
-        $('#past').hide();
-    });
-})
+$(document).ready(function () {
+    console.log("yo");
+  $(window).on("load", function () {
+      console.log("hi");
+    $(".loading").delay(800).fadeOut();
+  });
+
+  $("#past").hide();
+  $("#current").hide();
+  $(document).on("click", "#complete", function () {
+    this.style.color = "#cdc6a5";
+    $("#progress").css("color", "#424b54");
+    $("#past").show();
+    $("#current").hide();
+  });
+  $(document).on("click", "#progress", function () {
+    this.style.color = "#cdc6a5";
+    $("#complete").css("color", "#424b54");
+    $("#current").show();
+    $("#past").hide();
+  });
+});
