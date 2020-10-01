2119ef7599379fc999b21bb05fb8b913d88e466c
Jessica Xia <jessicax@uchicago.edu>
2020-09-30 22:11:07 -0500

    update about and color

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
 $(document).ready(function () {
-    console.log("yo");
   $(window).on("load", function () {
-      console.log("hi");
     $(".loading").delay(800).fadeOut();
   });
 
   $("#past").hide();
   $("#current").hide();
   $(document).on("click", "#complete", function () {
-    this.style.color = "#cdc6a5";
+    this.style.color = "white";
     $("#progress").css("color", "#424b54");
     $("#past").show();
     $("#current").hide();
   });
   $(document).on("click", "#progress", function () {
-    this.style.color = "#cdc6a5";
+    this.style.color = "white";
     $("#complete").css("color", "#424b54");
     $("#current").show();
     $("#past").hide();
